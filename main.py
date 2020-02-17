6fa44d7f4b0e74e9f64f4b0de2fe036081056382
GamerNoTitle <bili33@87ouo.top>
2020-02-17 16:38:40 +0800

    完成大部分功能，运行可能会出现编码错误的问题，正在修复……

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from array import array
 import time
 start_Pro=datetime.datetime.now()
 def create_csv(path):
-    with open(path,"w+",newline="",encoding="utf8") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
+    with open(path,"w+",newline="",encoding="gbk") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
         csv_file = csv.writer(file)
         head = heads # 创建csv表头
         csv_file.writerow(head)
 def append_csv(path):
-    with open(path,"a+",newline='',encoding="utf8") as file:
+    with open(path,"a+",newline='',encoding="gbk") as file:
         csv_file = csv.writer(file)
         data = [inputs]
         csv_file.writerows(data)
@@ -39,13 +39,14 @@ if(conf['comment_pub_date']): heads.append('comment_pub_date')
 # ========================================
 create_csv(path)    # 创建csv文件
 temp=array('i',[0]) # 去重复用的临时列表
+i = 1
 while True:
     if(i==num+1): break
     print("正在调用api……")
     res = r.get("https://www.mouse123.cn/api/163/api.php")  # 调用api
     data = res.json()   # 转化为json字符串
     temp_minus=len(temp)-1     # 获取temp中的元素数量，因为定义的时候给了一个元素，所以要剪掉这个元素
-        if temp_minus!=0:
+    if temp_minus!=0:
         t=1
         print("正在检测是否抓取过结果……")
         for t in range(len(temp)):
@@ -54,19 +55,35 @@ while True:
                 break
             elif(t==len(temp)-1):
                 print("未抓取过的结果，正在存入文件……")
-                # print(res.text)   # 输出一言，如需要把最前面的#去掉即可
+                inputs = [data['comment_id'],data["comment_nickname"]]
+                if(conf['song_id']): inputs.append(data['song_id'])
+                inputs.append(data['title'])
+                inputs.append(data['author'])
+                if(conf['album']): inputs.append(data['album'])
+                if(conf['description']): inputs.append(data['description'])
+                if(conf['pub_date']): inputs.append(data['pub_date'])
+                inputs.append(data['comment_content'])
+                if(conf['comment_user_id']): inputs.append(data['comment_user_id'])
+                if(conf['comment_pub_date']): inputs.append(data['comment_pub_date'])
                 append_csv(path)
-                temp.append(data["comment_id"])
+                temp.append(int(data["comment_id"]))
                 end_Pro=datetime.datetime.now()
                 print("已完成数量："+str(i)+'，已经用时：'+str(end_Pro-start_Pro))
                 i=i+1
                 break
     else:
-        inputs = [data['comment_id'],data['comment_username']]
+        inputs = [data['comment_id'],data["comment_nickname"]]
         if(conf['song_id']): inputs.append(data['song_id'])
-        # print(res.text) # 输出一言，如需要把最前面的#去掉即可
+        inputs.append(data['title'])
+        inputs.append(data['author'])
+        if(conf['album']): inputs.append(data['album'])
+        if(conf['description']): inputs.append(data['description'])
+        if(conf['pub_date']): inputs.append(data['pub_date'])
+        inputs.append(data['comment_content'])
+        if(conf['comment_user_id']): inputs.append(data['comment_user_id'])
+        if(conf['comment_pub_date']): inputs.append(data['comment_pub_date'])
         append_csv(path)
-        temp.append(data["comment_id"])
+        temp.append(int(data["comment_id"]))
         end_Pro=datetime.datetime.now()
         print("已完成数量："+str(i)+'，已经用时：'+str(end_Pro-start_Pro))
         i=i+1
