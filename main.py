7b210e2ff0c13f27d770a12a515835dc5331e033
GamerNoTitle <bili33@87ouo.top>
2020-02-17 16:44:10 +0800

    修改数据写入顺序

--- main.py
+++ main.py
@@ -25,13 +25,15 @@ path = conf['path'] # 设定输出路径/文件
 num = int(conf["times"])    # 设定抓取次数
 delay = int(conf["delay"])  # 设定抓取后延迟
 timeout = int(conf['timeout'])  # 设定超时时间
-heads = ['comment_id','comment_username']    # 创建表头，分别是评论ID、评论者昵称、歌曲名、歌曲作者、评论内容
+heads = []
 if(conf['song_id']): heads.append('song_id')
 heads.append('title')
 heads.append('author')
 if(conf['album']): heads.append('album')
 if(conf['description']): heads.append('description')
 if(conf['pub_date']): heads.append('pub_date')
+heads.append('comment_id')
+heads.append('comment_username')
 heads.append('content')
 if(conf['comment_user_id']): heads.append('comment_user_id')
 if(conf['comment_pub_date']): heads.append('comment_pub_date')
@@ -42,8 +44,9 @@ temp=array('i',[0]) # 去重复用的临时列表
 i = 1
 while True:
     if(i==num+1): break
+    time.sleep(delay)
     print("正在调用api……")
-    res = r.get("https://www.mouse123.cn/api/163/api.php")  # 调用api
+    res = r.get("https://www.mouse123.cn/api/163/api.php",timeout=timeout)  # 调用api
     data = res.json()   # 转化为json字符串
     temp_minus=len(temp)-1     # 获取temp中的元素数量，因为定义的时候给了一个元素，所以要剪掉这个元素
     if temp_minus!=0:
@@ -55,13 +58,14 @@ while True:
                 break
             elif(t==len(temp)-1):
                 print("未抓取过的结果，正在存入文件……")
-                inputs = [data['comment_id'],data["comment_nickname"]]
                 if(conf['song_id']): inputs.append(data['song_id'])
                 inputs.append(data['title'])
                 inputs.append(data['author'])
                 if(conf['album']): inputs.append(data['album'])
                 if(conf['description']): inputs.append(data['description'])
                 if(conf['pub_date']): inputs.append(data['pub_date'])
+                inputs.append(data['comment_id'])
+                inputs.append(data["comment_nickname"])
                 inputs.append(data['comment_content'])
                 if(conf['comment_user_id']): inputs.append(data['comment_user_id'])
                 if(conf['comment_pub_date']): inputs.append(data['comment_pub_date'])
@@ -72,13 +76,14 @@ while True:
                 i=i+1
                 break
     else:
-        inputs = [data['comment_id'],data["comment_nickname"]]
         if(conf['song_id']): inputs.append(data['song_id'])
         inputs.append(data['title'])
         inputs.append(data['author'])
         if(conf['album']): inputs.append(data['album'])
         if(conf['description']): inputs.append(data['description'])
         if(conf['pub_date']): inputs.append(data['pub_date'])
+        inputs.append(data['comment_id'])
+        inputs.append(data["comment_nickname"])
         inputs.append(data['comment_content'])
         if(conf['comment_user_id']): inputs.append(data['comment_user_id'])
         if(conf['comment_pub_date']): inputs.append(data['comment_pub_date'])
